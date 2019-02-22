4b1814341a49a4b51a3267769c33e6ac9f46fcb8
Karelys Souto <karelyssouto@icloud.com>
2019-02-22 11:09:57 -0500

    creted react app, started django app and currently working on models and RESTframework

---
+++ beautyCalendar/beautyCalendar_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BeautycalendarAppConfig(AppConfig):
+    name = 'beautyCalendar_app'
---
+++ beautyCalendar/beautyCalendar_app/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+class makeupArtist(models.Model):
+    name = models.CharField(max_length=50)
+    home
