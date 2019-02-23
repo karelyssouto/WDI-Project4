d6bf896ec5f82bc9dc327a66f63e37a43c9956f5
Karelys Souto <karelyssouto@icloud.com>
2019-02-22 22:09:09 -0500

    completed serializers

--- beautyCalendar/beautyCalendar_app/serializers.py
+++ beautyCalendar/beautyCalendar_app/serializers.py
@@ -11,11 +11,22 @@ from models import MakeupArtist, Appointment, Client
 #     class Meta:
 #         model = Meal
 #         fields = ('id', 'name', 'ingredients')
-class 
+
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = ('name', 'img', 'skinTon', 'skinType', 'clientId')
+
+class AppointmentSerializer(serializers.ModelSerializer):
+    client = ClientSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Appointment
+        fields = ('date', 'location', 'category', 'artistId', 'clientId')
 
 class MakeupArtistSerializer(serializers.ModelSerializer):
-    appointments = AppointmentSerializer(many= True)
+    appointments = AppointmentSerializer(many= True, read_only= True)
 
     class Meta:
         model = MakeupArtist
-        fields = ('id', 'name', 'hometown')
+        fields = ('artistId', 'name', 'hometown')
